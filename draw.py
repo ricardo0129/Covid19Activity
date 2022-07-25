43a1bb0069d62a374f92b95ae4df84a2a6291781
Ricardo Ruiz <ricky@Ricardos-MacBook-Air.local>
2022-07-25 13:49:33 -0400

    cleaned

--- draw.py
+++ draw.py
@@ -32,8 +32,15 @@ def draw_map(f):
             previous = [x,y]
 
 def scale_value(x,min,max):
-    a,b = 0.4,1.0
+    a,b = 0.3,1.2
     return (b-a)*(x-min)/(max-min)+a
+
+def draw_scale(mini,maxi):
+    delta = (maxi-mini)/4
+    for i in range(4):
+        v = scale_value(delta*(i+1),mini,maxi)
+        stddraw.circle(x0+4+4*i,y0+5,max(v,0))
+        stddraw.text(x0+4+4*i,y0+3,str(delta*(i+1)))
 
 def show_cases(day):
     data = map_data(day)
@@ -43,17 +50,20 @@ def show_cases(day):
     for d in data:
         mini = min(mini,d[0])
         maxi = max(maxi,d[0])
+    
+    draw_scale(mini,maxi)
     for d in data:
         v = scale_value(d[0],mini,maxi)
         #stddraw.setPenRadius(max(v,0))
         stddraw.circle(d[2],d[1],max(v,0))
 
+
 def show_map():
     stddraw.setPenColor(stddraw.BLACK)
     stddraw.setFontSize(16)
-    stddraw.text(x0+10,y0+1.4,"Covid Cases per State") 
-    stddraw.show(1000.0)
+    stddraw.text(x0+10,y0+1.4,"Relative Covid Cases per State") 
+    stddraw.show()
 
 draw_map("usa_map.txt")
-show_cases("03-10-2021")
+show_cases("01-22-2022")
 show_map()
